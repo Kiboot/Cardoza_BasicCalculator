8a7f51e2f12d219f672eee51eda2b12b8c3b16f1
Prg <kevinlee.cardoza@gmail.com>
2021-01-20 09:59:10 +0800

    Initial commit with declared XML attributes

---
+++ app/src/main/java/mcm/edu/ph/cardoza_basiccalculator/MainActivity.java
@@ -0,0 +1,23 @@
+package mcm.edu.ph.cardoza_basiccalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnAdd, btnSub, btnDiv, btnMul, btnMod;
+
+
+
+        
+
+
+    }
+}
